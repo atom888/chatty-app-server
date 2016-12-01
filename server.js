e993ba8302519e29a0efe8df29dfad5fe502e876
Adam <adam.dipinto@gmail.com>
2016-12-01 02:41:29 +0000

    Sending stringified parsed message to App.jsx. through client - broadcasting

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
-
+const uuid = require('node-uuid');
 // Set the port to 4000
 const PORT = 4000;
 
@@ -18,15 +18,23 @@ const wss = new SocketServer({ server });
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
-
   ws.on('message', function(event) {
   let parsedMessage = JSON.parse(event);
-  console.log("Incoming Message", parsedMessage.content);
-});
 
+    const parsedNewMessage = {
+      id: uuid.v4(parsedMessage.id),
+      username: parsedMessage.username,
+      content: parsedMessage.content
+    }
+
+    wss.clients.forEach(function each(client) {
+      client.send(JSON.stringify(parsedNewMessage));
+    });
+  });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
